50fc0eeb61b4a55e78b42590b35bb1ee4f7d2b40
AndyPark20 <apark1214@gmail.com>
2022-12-02 15:47:16 -0800

    working on calculating action taken from compeititors

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -15,21 +15,28 @@
 
 
 const data = require('./stratGuide/data');
-let finalProductionData ={opponent:[],me:[]};
+let finalProductionData = {opponent:{action:[],score:0},me:{action:[],score:0}};
+
+
 
 //function to convert json data into formatted object  for easier accessability
-const formatData =(unformattedData)=>{
+const formatData = (unformattedData) => {
   splitCharactersArray = [];
-  splitCharactersArray =unformattedData.stratGuide.split(",");
+  splitCharactersArray = unformattedData.stratGuide.split(",");
   
   //Loop thru the splitCharacterArray and if the value index is even = opponent, if odd = me
   splitCharactersArray.forEach((values,index)=>{
-    index % 2 !== 1 ? finalProductionData.opponent.push(values) : finalProductionData.me.push(values)
-  })
+    index % 2 !== 1 ? finalProductionData.opponent.action.push(values) : finalProductionData.me.action.push(values)
+  });
+};
+
+
+formatData(data);
+
+//Function to calculate the action taken from competitors (rock, paper, and scissor values)
+const actionComparison = () => {
+
 }
 
+//Function determine the values of presented action and the outcome points
 
-formatData(data)
-
-console.log('oppponent', finalProductionData.opponent.length);
-console.log('me',finalProductionData.me.length)
