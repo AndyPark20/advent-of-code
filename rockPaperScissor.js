eef02a7478e073634083c5db3e51a70f366c19b0
AndyPark20 <apark1214@gmail.com>
2022-12-02 14:14:45 -0800

    reading the main objectives and explanations

---
+++ rockPaperScissor.js
@@ -0,0 +1,14 @@
+//Rock Paper Scissor game
+  //Rules:
+    //Can only chose between rock, paper, scissor (points are 1, 2, 3 respectively)
+    //Won = 6
+    //Tie = 3
+    //Loss = 0
+
+    //X,Y,Z = Rock, paper, scissor (For me)
+    //A,B,C = Rock, paper, scissor (Opponent)
+
+  //Objective
+    //There is a guide that the elves have given that shows the prediction of each players options
+    //What is the total point for me based on the strategy guide. 
+ 
