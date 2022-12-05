21f995d794fe184a4974cec7557b33da3b0cbbe6
AndyPark20 <apark1214@gmail.com>
2022-12-05 15:00:36 -0800

    running character test

--- backpack.js
+++ backpack.js
@@ -1,5 +1,7 @@
+const first = require("ee-first");
+
 // const data = require('./stratGuide/backPackData');
-const data ='rPWmLCTCQlCQQmmrWLrQShJshhzdhhJjcSjlzRds';
+const data =['rPWmLCTCQlCQQmmrWLrQShJshhzdhhJjcSjlzRds'];
 //Psudo code
 //Loop thru the master array and split the characters by half.
 //assign the characters into small pocket and big pocket
@@ -18,11 +20,15 @@ const checkBackPack =()=>{
       };
     });
   });
+  console.log('firstPocket', firstPocket);
+  console.log('secondPocket', secondPoket);
 };
 
 const checkAlphabet =()=>{
   const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   const lowercase = upperCase.map(values=>{return values.toLowerCase()});
 }
+
+checkBackPack();
 
 
