e4eda05beec73f9c8f8f271eb3df7e01aceecef8
AndyPark20 <apark1214@gmail.com>
2022-12-05 16:51:07 -0800

    in progress

--- backpack.js
+++ backpack.js
@@ -1,9 +1,8 @@
 const first = require("ee-first");
 
 // const data = require('./stratGuide/backPackData');
-const data = ['rPWmLCTCQlCQQmmrWLrQShJshhzdhhJjcSjlzRds'];
+const data = ['ZCWhhCsJCzSJzSbzgsmPTGNNPPNGjgLTLjgn'];
 
-let totalCount =0;
 
 const checkBackPack =()=>{
 
@@ -18,20 +17,29 @@ const checkBackPack =()=>{
         secondPocket.push(characters);
       };
     });
-    console.log('firstPocket', firstPocket);
-    console.log('secondPocket', secondPocket);
     checkAlphabet(firstPocket, secondPocket);
   });
 };
 
 const checkAlphabet =(firstPocket, secondPocket)=>{
+  console.log('first', firstPocket);
+  console.log('second', secondPocket);
+  let totalCount =0;
   const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   const lowerCase = upperCase.map(values=>{return values.toLowerCase()});
-  firstPocket.forEach((firstPocketChar, firstPocketCharIndex)=>{
+  console.log('lowercase', lowerCase.indexOf('g'))
+  firstPocket.forEach((firstPocketChar,indexPosition)=>{
     if(secondPocket.indexOf(firstPocketChar) !== -1){
-      console.log(firstPocket[secondPocket.indexOf(firstPocketChar)]);
+      const foundCharacter = secondPocket[secondPocket.indexOf(firstPocketChar)];
+      console.log('uppercase', foundCharacter);
+      if(upperCase.indexOf(foundCharacter)){
+        totalCount += (upperCase.length + upperCase.indexOf(foundCharacter)+1);
+      }else if (lowerCase.indexOf(foundCharacter)){
+        totalCount += (lowerCase.indexOf(foundCharacter)+1);
+      }
     }
   });
+  console.log(totalCount);
 };
 
 checkBackPack();
